fec31f55547d847b8f573323d73842fd1a1c5739
Luan Campos <luancamposdeveloper@gmail.com>
2025-07-18 07:10:08 -0300

    chore: get slug with params

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,6 @@
-import React from "react";
-
-const Slug = () => {
-  return <div>Slug</div>;
+const Slug = async ({params}: {params: Promise<{ slug: string}>}) => {
+  const {slug} = await params
+  return <div>{slug}</div>;
 };
 
 export default Slug;
