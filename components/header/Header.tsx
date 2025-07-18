e2e47bcc1cd0464224c9134c715d7c0e606ceed0
Luan Campos <luancamposdeveloper@gmail.com>
2025-07-18 07:09:49 -0300

    chore: add logo with link

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { SearchInput } from "@/components/header/search-input";
 import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <nav className="bg-background/80 supports-[backdrop-filter]:bg-background/60 fixed top-0 right-0 left-0 z-50 flex items-center justify-between p-6 shadow-lg backdrop-blur lg:px-20">
       {" "}
-      <span className="logo text-2xl font-bold">
+      <Link href="/" className="logo text-2xl font-bold">
         Cotton<span className="text-green-500">Byte</span>News
-      </span>
+      </Link>
       <SearchInput />
       <a
         href="https://github.com/luancamposdev"
